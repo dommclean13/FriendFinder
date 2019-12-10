d6485bd72b11bd9f6c4b9fc44188705259b54eb1
Dominique McLean <dommclean13@gmail.com>
2019-12-10 17:30:00 -0600

    changes2

---
+++ app/data/friends.js
@@ -0,0 +1,90 @@
+let friends = [ {
+    "name": "Barbra",
+    "photo": "https://pbs.twimg.com/profile_images/2737691608/24a1c2b3c88bb3750a00f709520b8650_400x400.jpeg",
+    "score": [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+        
+    ]
+},
+{
+    "name": "Bulk",
+    "photo": "https://www.chaostrophic.com/wp-content/uploads/2016/08/celebs-31.jpg",
+    "score": [
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2"
+        
+    ]
+},
+{
+    "name": "Steve",
+    "photo": "https://www.celebritynetworth123.com/wp-content/plugins/networthdisplay/cnwimages/s/steve-harvey.jpg",
+    "score": [
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3"
+
+       
+    ]
+},
+{
+    "name": "Kanye",
+    "photo": "https://cdn.ebaumsworld.com/mediaFiles/picture/2266412/84679528.jpg",
+    "score": [
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4"
+        
+    ]
+},
+{
+    "name": "Jen",
+    "photo": "https://www.askideas.com/media/36/Funny-Photoshop-Jennifer-Aniston-Face-Image.jpg",
+    "score": [
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5"
+        
+    ]
+},
+
+];
+
+module.exports = friends;
